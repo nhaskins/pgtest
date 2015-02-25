88954c5cbf0e9061b0dcfd25fd6cecf5535de4b1
Nathan A Haskins <nathan@haskins.it>
2015-02-25 10:38:56 -0600

    trying jquery refactor

--- js/index.js
+++ js/index.js
@@ -1,33 +1,42 @@
 
-var app = {
+// var app = {
 
-    initialize: function() {
-        this.bindEvents();
-    },
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+//     initialize: function() {
+//         this.bindEvents();
+//     },
+//     bindEvents: function() {
+//         document.addEventListener('deviceready', this.onDeviceReady, false);
+//     },
+//     onDeviceReady: function() {
+//         app.receivedEvent('deviceready');
+//     },
+//     receivedEvent: function(id) {
+//         var parentElement = document.getElementById(id);
+//         var listeningElement = parentElement.querySelector('.listening');
+//         var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+//         listeningElement.setAttribute('style', 'display:none;');
+//         receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
-        alert("device ready pass!!");
-        // var url = "http://maps.google.com/maps?daddr=51 N Gore Ave. Webster Groves, MO 63119"
-        // var url = "mailto:nathan@haskins.it"
-        var url = "https://twitter.com/alkayme";
-        var ref = window.open(url, '_system', 'location=yes');
-        // ref.addEventListener('loadstart', function() { alert(event.url); });
-    }
-};
+//         console.log('Received Event: ' + id);
+//         alert("device ready pass!!");
+//         // var url = "http://maps.google.com/maps?daddr=51 N Gore Ave. Webster Groves, MO 63119"
+//         // var url = "mailto:nathan@haskins.it"
+//         var url = "https://twitter.com/alkayme";
+//         var ref = window.open(url, '_system', 'location=yes');
+//         // ref.addEventListener('loadstart', function() { alert(event.url); });
+//     }
+// };
 
 jQuery(document).ready(function($){
     console.log("foobar");
+    addEventListener('deviceready', onDeviceReady, false);
+
+    function onDeviceReady(){
+        receivedEvent('deviceready');
+    }
+    function receivedEvent(id){
+        var url = "https://twitter.com/alkayme";
+        var ref = window.open(url, '_system', 'location=yes');
+    }
 });
